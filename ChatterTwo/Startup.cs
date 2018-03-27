aa781af50ed31796b9501985d87588613e502cb2
Weiskri <kkweis13@gmail.com>
2018-03-27 14:08:26 -0400

    first commit

---
+++ ChatterTwo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChatterTwo.Startup))]
+namespace ChatterTwo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
